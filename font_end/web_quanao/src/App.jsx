3ff3f4886b2e823669aeac2ce6c18fce9fcd86b5
lonezu03 <118800029+lonezu03@users.noreply.github.com>
2024-10-23 19:07:51 +0700

    thanhvu/10232024addcomponent

---
+++ font_end/web_quanao/src/App.jsx
@@ -0,0 +1,29 @@
+
+import './App.css'
+import Menu from './component/menu.jsx'
+import Banner from './component/baner.jsx'
+import ListSP from './component/listSP.jsx'
+import Login from './component/login.jsx'
+import Dangky from './component/dangky.jsx'
+import Footer2 from './component/footer2.jsx'
+import DetailItem from './component/chitietSP.jsx'
+import Description from './component/description.jsx';
+
+function App() {
+
+  return (
+    <div className='static'>
+    <div className='static'><Menu/></div>
+     <div className='relative translate-y-20'><Banner/></div>
+     <div><ListSP/></div>
+     <div><Login/></div>
+     <div><Dangky/></div>
+     {/* <div><Footer/></div> */}
+     <div><Footer2/></div>
+     <div><DetailItem/></div>
+     <div><Description/></div>
+    </div>
+  )
+}
+
+export default App
